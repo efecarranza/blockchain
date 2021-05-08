55372f8312853f318364ce995b22acf8420e1738
piscu Carranza <fcarr004@fiu.edu>
2021-05-08 18:34:03 -0300

    sign transaction

--- backend/wallet/wallet.py
+++ backend/wallet/wallet.py
@@ -1,6 +1,10 @@
+import json
 import uuid
 
 from backend.config import STARTING_BALANCE
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives.asymmetric import ec
+from cryptography.hazmat.primitives import hashes
 
 class Wallet:
     """
@@ -11,3 +15,22 @@ class Wallet:
     def __init__(self):
         self.address = str(uuid.uuid4())
         self.balance = STARTING_BALANCE
+        self.private_key = ec.generate_private_key(ec.SECP256K1(), default_backend())
+        self.public_key = self.private_key.public_key()
+
+    def sign(self, data):
+        """
+        Generates a signature based on the data using the local private key.
+        """
+        return self.private_key.sign(json.dumps(data).encode('utf-8'), ec.ECDSA(hashes.SHA256()))
+
+def main():
+    wallet = Wallet()
+    print(f'wallet: {wallet.__dict__}')
+
+    data = {'test': 'testing'}
+    signature = wallet.sign(data)
+    print(f'signature: {signature}')
+
+if __name__ == '__main__':
+    main()
